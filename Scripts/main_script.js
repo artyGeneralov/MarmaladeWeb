7ce28cf8ef5b90091c4b4c6fd0329a86cb44b05d
ArtyGeneralov <artyom1100@gmail.com>
2023-04-20 17:02:30 +0300

    style change in js script

--- Scripts/main_script.js
+++ Scripts/main_script.js
@@ -1,20 +1,26 @@
 /* flasher */
-let about_book = `Join Marmalade, an adventurous little mouse, as she sets off
+let about_book = `<i>Join Marmalade, an adventurous little mouse, as she sets off
 					on a thrilling quest to find the ancient Mouse Kingdom\'s treasure. 
 					Along the way, she befriends a wise owl, a brave hedgehog, 
 					and a clever fox. Together, they overcome obstacles, solve riddles, 
 					and learn the true power of friendship. 
 					Will Marmalade and her friends uncover the long-lost treasure? 
-					Dive into this enchanting tale to find out!`;
-let about_author = `The author of \"The Incredible Journey of Marmalade the Mouse\" 
+					Dive into this enchanting tale to find out!</i>`;
+let about_author = `<i>The author of \"The Incredible Journey of Marmalade the Mouse\" 
 					loves to weave delightful stories that spark the imagination of young readers. 
 					With a unique knack for dreaming up whimsical adventures, 
 					the author draws upon a vast treasure trove of knowledge and creativity. 
 					Their curiosity knows no bounds, and their ability to connect with 
 					children through enchanting tales has made them a beloved storyteller 
-					among both kids and adults alike.`;
+					among both kids and adults alike.</i>`;
 let duration = 1000;
 let btns = document.getElementsByClassName("flashing_btn");
+
+window.onload = (event) => {
+  document.querySelector('#info_content').innerHTML = about_book;
+};
+
+
 for(let i = 0; i < btns.length; i++){
 	btns[i].onclick = () => {
 		event.preventDefault;
